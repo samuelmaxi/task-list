64484524ecbc724f8d758f12e4ef1f1867937e6a
samuelmaxi <samuelgaraujo87@gmail.com>
2023-08-16 19:14:55 -0300

    feat: add functionality to add and delete tasks

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Code to hide the Label when input is selected
 document.addEventListener("DOMContentLoaded", function () {
   const input = document.querySelector('.name-task');
   const label = document.querySelector('.text-input');
@@ -12,3 +13,47 @@ document.addEventListener("DOMContentLoaded", function () {
     };
   });
 });
+
+
+// Code to add tasks to list
+const taskList = []
+const listElemet = document.querySelector(".list")
+let ulElement = document.createElement("ul")
+const btn = document.querySelector(".chekbox")
+
+btn.addEventListener("click", function () {
+  const titleTask = document.querySelector(".name-task").value
+  const task = {
+    title: titleTask
+  }
+  taskList.push(task)
+  ulElement.textContent = ''
+
+  taskList.forEach(taskItens => {
+    const liElement = document.createElement("li")
+    liElement.textContent = `${taskItens.title}`
+    ulElement.appendChild(liElement)
+  })
+  document.querySelector('.name-task').value = ''
+})
+
+listElemet.appendChild(ulElement)
+
+// Delete last task
+const bntDelete = document.querySelector('.delete')
+
+bntDelete.addEventListener('click', function () {
+  if (taskList.length > 0) {
+    taskList.pop()
+
+    ulElement.textContent = ''
+
+    taskList.forEach(taskItens => {
+      const liElement = document.createElement('li')
+      liElement.textContent = `${taskItens.title}`
+      ulElement.appendChild(liElement)
+    })
+  }
+
+})
+
